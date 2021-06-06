12080753510cd590e66962ec26e22efda2f21002
ianahmfachriza <me.business25@gmail.com>
2021-06-06 14:22:47 +0700

    Add API Path for storing all the document path in firestore. Simplify createJob method

--- lib/services/database.dart
+++ lib/services/database.dart
@@ -1,5 +1,6 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:time_tracker/models/job.dart';
+import 'package:time_tracker/services/api_path.dart';
 
 abstract class Database {
   Future<void> createJob(Job job);
@@ -12,10 +13,16 @@ class FirestoreDatabase implements Database {
   });
 
   @override
-  Future<void> createJob(Job job) async {
-    final path = '/users/$uid/jobs';
-    final collectionRef = FirebaseFirestore.instance.collection(path);
+  Future<void> createJob(Job job) async => _setData(
+        path: APIPath.job(uid, 'job_abc'),
+        data: job.toMap(),
+      );
 
-    await collectionRef.doc().set(job.toMap());
+  Future<void> _setData({
+    required String path,
+    required Map<String, dynamic> data,
+  }) async {
+    final ref = FirebaseFirestore.instance.doc(path);
+    await ref.set(data);
   }
 }
