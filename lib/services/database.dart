d42e141b9d84b4a29a525d569dc92c5ac6bad514
ianahmfachriza <me.business25@gmail.com>
2021-06-07 20:35:29 +0700

    Adding a FirestoreService Class and make it singleton with private constructor and instance static method

--- lib/services/database.dart
+++ lib/services/database.dart
@@ -1,6 +1,6 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:time_tracker/models/job.dart';
 import 'package:time_tracker/services/api_path.dart';
+import 'package:time_tracker/services/firestore_service.dart';
 
 abstract class Database {
   Future<void> createJob(Job job);
@@ -13,43 +13,17 @@ class FirestoreDatabase implements Database {
     required this.uid,
   });
 
+  final _service = FirestoreService.instance;
+
   @override
-  Future<void> createJob(Job job) async => _setData(
+  Future<void> createJob(Job job) async => _service.setData(
         path: APIPath.job(uid, 'job_abc'),
         data: job.toMap(),
       );
 
   @override
-  Stream<List<Job>> jobsStream() => _collectionStream(
+  Stream<List<Job>> jobsStream() => _service.collectionStream(
         path: APIPath.jobs(uid),
         builder: (data) => Job.fromMap(data),
       );
-
-  Future<void> _setData({
-    required String path,
-    required Map<String, dynamic> data,
-  }) async {
-    try {
-      final ref = FirebaseFirestore.instance.doc(path);
-      await ref.set(data);
-    } on FirebaseException catch (e) {
-      throw e.message.toString();
-    } catch (e) {
-      throw e.toString();
-    }
-  }
-
-  Stream<List<T>> _collectionStream<T>({
-    required String path,
-    required T Function(Map<String, dynamic> data) builder,
-  }) {
-    final reference = FirebaseFirestore.instance.collection(path);
-    final snapshot = reference.snapshots();
-    return snapshot.map(
-      (event) => event.docs.map((job) {
-        final data = job.data();
-        return builder(data);
-      }).toList(),
-    );
-  }
 }
---
+++ lib/services/firestore_service.dart
@@ -0,0 +1,35 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+class FirestoreService {
+  FirestoreService._();
+
+  static final instance = FirestoreService._();
+
+  Future<void> setData({
+    required String path,
+    required Map<String, dynamic> data,
+  }) async {
+    try {
+      final ref = FirebaseFirestore.instance.doc(path);
+      await ref.set(data);
+    } on FirebaseException catch (e) {
+      throw e.message.toString();
+    } catch (e) {
+      throw e.toString();
+    }
+  }
+
+  Stream<List<T>> collectionStream<T>({
+    required String path,
+    required T Function(Map<String, dynamic> data) builder,
+  }) {
+    final reference = FirebaseFirestore.instance.collection(path);
+    final snapshot = reference.snapshots();
+    return snapshot.map(
+      (event) => event.docs.map((job) {
+        final data = job.data();
+        return builder(data);
+      }).toList(),
+    );
+  }
+}
