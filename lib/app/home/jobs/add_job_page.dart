bc648110a8281d44280352eb775f414c05048478
ianahmfachriza <me.business25@gmail.com>
2021-06-08 21:03:49 +0700

    Adding Form with TextFormField for adding the job and rate per hour

--- lib/app/home/jobs/add_job_page.dart
+++ lib/app/home/jobs/add_job_page.dart
@@ -17,27 +17,93 @@ class AddJobPage extends StatefulWidget {
 }
 
 class _AddJobPageState extends State<AddJobPage> {
+  final _formKey = GlobalKey<FormState>();
+
+  String? _name;
+  int? _ratePerHour;
+
+  bool _validateAndSaveForm() {
+    final form = _formKey.currentState!;
+    if (!form.validate()) return false;
+    form.save();
+    return true;
+  }
+
+  void _submit() {
+    if (_validateAndSaveForm()) {
+      print('Job Name: $_name');
+      print('Rate per hour: $_ratePerHour');
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('New Job'),
+        actions: [
+          TextButton(
+            onPressed: _submit,
+            child: Text(
+              'Save',
+              style: TextStyle(fontSize: 18),
+            ),
+            style: TextButton.styleFrom(primary: Colors.white),
+          )
+        ],
       ),
       body: _buildContent(),
     );
   }
-}
 
-Widget _buildContent() {
-  return SingleChildScrollView(
-    child: Card(
-      margin: const EdgeInsets.all(16),
-      child: Padding(
-        padding: const EdgeInsets.all(16),
-        child: Placeholder(
-          fallbackHeight: 200,
+  Widget _buildContent() {
+    return SingleChildScrollView(
+      child: Card(
+        margin: const EdgeInsets.all(16),
+        child: Padding(
+          padding: const EdgeInsets.all(16),
+          child: _buildForm(),
         ),
       ),
-    ),
-  );
+    );
+  }
+
+  Widget _buildForm() {
+    return Form(
+      key: _formKey,
+      child: Column(
+        children: _buildFormChildren(),
+      ),
+    );
+  }
+
+  List<Widget> _buildFormChildren() {
+    return [
+      TextFormField(
+        onSaved: (newValue) => _name = newValue,
+        validator: (value) {
+          if (value!.isEmpty) return 'Job name cannot be empty';
+          return null;
+        },
+        decoration: InputDecoration(
+          labelText: 'Job Name',
+        ),
+      ),
+      SizedBox(height: 8),
+      TextFormField(
+        onSaved: (newValue) => _ratePerHour = int.parse(newValue.toString()),
+        validator: (value) {
+          if (value!.isEmpty) return 'Rate hour cannot be empty';
+          return null;
+        },
+        keyboardType: TextInputType.numberWithOptions(
+          signed: false,
+          decimal: false,
+        ),
+        decoration: InputDecoration(
+          labelText: 'Rate per Hour',
+        ),
+      ),
+    ];
+  }
 }
