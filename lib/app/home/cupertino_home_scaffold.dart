e0b6453937896c01ed13438335ae957c17504e13
ianahmfachriza <me.business25@gmail.com>
2021-07-03 14:30:45 +0700

    Creating homepage with selected tab

---
+++ lib/app/home/cupertino_home_scaffold.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+import 'package:time_tracker/app/home/tab_item.dart';
+
+class CupertinoHomeScaffold extends StatelessWidget {
+  final TabItem currentTab;
+  final ValueChanged<TabItem> onSelectTab;
+  const CupertinoHomeScaffold({
+    Key? key,
+    required this.currentTab,
+    required this.onSelectTab,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
---
+++ lib/app/home/home_wrapper.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:time_tracker/app/home/cupertino_home_scaffold.dart';
+import 'package:time_tracker/app/home/tab_item.dart';
+
+class HomeWrapper extends StatefulWidget {
+  const HomeWrapper({Key? key}) : super(key: key);
+
+  @override
+  _HomeWrapperState createState() => _HomeWrapperState();
+}
+
+class _HomeWrapperState extends State<HomeWrapper> {
+  TabItem _currentTab = TabItem.jobs;
+  @override
+  Widget build(BuildContext context) {
+    return CupertinoHomeScaffold(
+      currentTab: _currentTab,
+      onSelectTab: (item) {},
+    );
+  }
+}
